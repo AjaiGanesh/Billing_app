bb922daa6a2c11dd57ee7b0bb60f2bb74f3976a2
AjaiGanesh <ajaiganesh09@gmail.com>
2025-06-22 12:05:53 +0530

    chore: add DB level constraints for Products

--- db/migrate/20250622060404_create_products.rb
+++ db/migrate/20250622060404_create_products.rb
@@ -1,13 +1,14 @@
 class CreateProducts < ActiveRecord::Migration[7.1]
   def change
     create_table :products do |t|
-      t.string :name
-      t.string :product_code
-      t.integer :stock
-      t.float :price
-      t.float :tax
+      t.string :name, null: false
+      t.string :product_code, null: false
+      t.integer :stock, null: false, default: 0
+      t.float :price, null: false
+      t.float :tax, null: false
 
       t.timestamps
     end
+    add_index :products, :product_code, unique: true
   end
 end
