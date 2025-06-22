98d9e549c2dec3fadef663762870e3c2555d1c27
AjaiGanesh <ajaiganesh09@gmail.com>
2025-06-22 12:25:34 +0530

    feat: add validations to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+  validates :name, :product_code, :stock, :price, :tax, presence: true
+  validates :product_code, uniqueness: { case_sensitive: false }
+  validates :stock, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    message: "must be 0 or more"
+  }
+  validates :price, numericality: {
+    greater_than: 0,
+    message: "must be greater than 0"
+  }
+  validates :tax, numericality: {
+    greater_than_or_equal_to: 0,
+    message: "must be 0 or more"
+  }
+
 end
